46ea4b9895f39fe691c25e94c9edab14e622cacc
qiaodan-cuhk <danqiao@link.cuhk.edu.cn>
2024-08-26 21:35:33 +0800

    scaffold

--- src/modules/doe/__init__.py
+++ src/modules/doe/__init__.py
@@ -1,18 +1,17 @@
 from .joint_mlp_class import JointMLPClassifier
 from .mlp_class import MLPClassifier
+from .llm_class import LLMlassifier
 
 REGISTRY = {}
 
 REGISTRY["mlp"] = MLPClassifier
 REGISTRY["joint_mlp"] = JointMLPClassifier
+REGISTRY["llm_mlp"] = LLMlassifier
 
 
 def doe_classifier_config_loader(cfg, ids):
     type = cfg.type
-    registry = {
-        "MLP": MLPClassifier,
-        }
-    cls = registry[type]
+    cls = REGISTRY[type]
     if not hasattr(cls, "from_config"):
         raise NotImplementedError(f"There is no from_config method defined for {type}")
     else:
